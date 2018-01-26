a13c072c3e80026ae86ec464c212d70d043b8478
Sidney Reis <sidney.reis@acad.pucrs.br>
2018-01-26 00:23:23 -0200

    :sparkles: Audio and log POC finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
 using System.Collections;
+using System;
+using System.IO;
 
 public class PlayerMovement : MonoBehaviour {
   private SpriteRenderer sprite;
   private Animator anim;
+  private AudioSource audioSrc;
+
+  static void WriteToFile(string filePath, string textToWrite)
+  {
+    using (StreamWriter outputFile = new StreamWriter(filePath, true))
+    {
+      outputFile.WriteLine(textToWrite);
+    }
+  }
 
   void Start()
   {
     sprite = gameObject.GetComponent<SpriteRenderer>();
     anim = GetComponent<Animator>();
+    audioSrc = gameObject.GetComponent<AudioSource>();
   }
 
   void Update()
@@ -42,6 +54,21 @@ public class PlayerMovement : MonoBehaviour {
       position.x = (float)(position.x + 0.1);
       this.transform.position = position;
       sprite.flipX = false;
+    }
+
+    if (Input.GetKey(KeyCode.M))
+    {
+      audioSrc.Stop();
+    }
+
+    if (Input.GetKey(KeyCode.P))
+    {
+      audioSrc.Play();
+    }
+
+    if (Input.GetKey(KeyCode.L))
+    {
+      WriteToFile("historico.txt", "test");
     }
 
     if ((!Input.GetKey(KeyCode.W)) & (!Input.GetKey(KeyCode.A)) & (!Input.GetKey(KeyCode.S)) & (!Input.GetKey(KeyCode.D)))
