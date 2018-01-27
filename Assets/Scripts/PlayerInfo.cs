5b4493f4a7a89d44a88d64f2c5fadadfb8cb92d5
Sidney Reis <sidney.reis@acad.pucrs.br>
2018-01-27 21:58:13 -0200

    :sparkles: Quit game

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerInfo : MonoBehaviour {
-  public static int selectedSpecies = 0; // TODO: Trocar para -1
+  public static int selectedSpecies = -1; // TODO: Trocar para -1
+  public static int chestsFound = 0;
+  public static int CHESTS_TO_WIN = 6;
 
   void Awake() {
     DontDestroyOnLoad(transform.gameObject);
---
+++ Assets/Scripts/Quitgame.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitgame : MonoBehaviour {
+	public void quit() {
+		Application.Quit();
+	}
+}
