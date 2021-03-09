aea59e2fa1616e084b2cbdcc7393bb768af8668f
Diogo45 <diogohartmann45@gmail.com>
2021-03-09 18:40:49 -0300

    Upgrade to unity 2019.4 LTS; Upgrade to new Terrain Layers

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,47 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-  private SpriteRenderer sprite;
-  private Animator anim;
-  private CharacterController controller;
+public class PlayerMovement : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private CharacterController controller;
 
-  void Start()
-  {
-    sprite = gameObject.GetComponent<SpriteRenderer>();
-    anim = GetComponent<Animator>();
-    controller = GetComponent<CharacterController>();
-  }
-
-  void Update()
-  {
-    if (PlayerInfo.chestBeingPlayed == -1)
+    void Start()
     {
-      var x = Input.GetAxis("Horizontal") * Time.deltaTime * 30.0f;
-      var z = Input.GetAxis("Vertical") * Time.deltaTime * 30.0f;
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
 
-      if (x != 0 || z != 0)
-      {
-        if (x < 0)
+    void Update()
+    {
+        if (PlayerInfo.chestBeingPlayed == -1)
         {
-          sprite.flipX = true;
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 30.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 30.0f;
+
+            if (x != 0 || z != 0)
+            {
+                if (x < 0)
+                {
+                    sprite.flipX = true;
+                }
+                else
+                {
+                    sprite.flipX = false;
+                }
+                anim.SetBool("walking", true);
+                controller.Move(new Vector3(x, 0, z));
+            }
+            else
+            {
+                anim.SetBool("walking", false);
+            }
         }
         else
         {
-          sprite.flipX = false;
+            anim.SetBool("walking", false);
         }
-        anim.SetBool("walking", true);
-        controller.Move(new Vector3(x, 0, z));
-      }
-      else
-      {
-        anim.SetBool("walking", false);
-      }
     }
-    else
-    {
-      anim.SetBool("walking", false);
-    }
-  }
 }
