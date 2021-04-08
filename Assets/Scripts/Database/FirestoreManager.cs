66405daec48f9290fd71c638a5e19c4a6ca556b2
Diogo45 <diogohartmann45@gmail.com>
2021-04-08 14:16:09 -0300

    included RestClient for firebase database communication

---
+++ Assets/Scripts/Database/FirestoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirestoreManager : MonoBehaviour
+{
+
+    [SerializeField] private RegisterData _parentData;
+    [SerializeField] private RegisterData _parentTeenData;
+    [SerializeField] private RegisterData _teenData;
+
+    [SerializeField] private RegisterData _loginData;
+
+    public void WriteRegisterData()
+    {
+
+    }
+
+    public void GetLoginData()
+    {
+
+    }
+
+
+}
