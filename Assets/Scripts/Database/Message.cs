2f23b1c132f70d72c46c82bf4f16df8163d86fbd
Diogo45 <diogohartmann45@gmail.com>
2021-06-10 15:07:17 -0300

    Message System

---
+++ Assets/Scripts/Database/Message.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : ScriptableObject
+{
+    public string Id;
+    public string message;
+    public string httpResponse;
+}
