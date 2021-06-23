303968146ac36f197587194b9f4f53531f3f3cbd
Diogo45 <diogohartmann45@gmail.com>
2021-06-23 13:16:38 -0300

    a

--- Assets/VideoManager.cs
+++ Assets/VideoManager.cs
@@ -8,25 +8,38 @@ public class VideoManager : MonoBehaviour
 {
     [SerializeField] private Button _nextButton;
     [SerializeField] private VideoPlayer _videoPlayer;
+    [SerializeField] private GameObject _videoScreen;
+    [SerializeField] private GameObject _expScreen;
 
     private void Start()
     {
-        _nextButton.gameObject.SetActive(false);
-        //
-        StartCoroutine(WaitForVideoEnd());
+           
+        
     }
 
     private IEnumerator WaitForVideoEnd()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitUntil(() =>_videoPlayer.isPrepared);
 
         var time = _videoPlayer.frameCount / _videoPlayer.frameRate;
 
         Debug.Log("Video time is: " + time);
 
-        yield return new WaitForSeconds(time);
+        yield return new WaitForSeconds(time - 5f);
 
         EmotionHuntersController.instance.ToECT();
+
+        gameObject.SetActive(false);
+
+    }
+
+    public void ToVideo()
+    {
+        _nextButton.gameObject.SetActive(false);
+        _expScreen.SetActive(false);
+        _videoScreen.SetActive(true);
+
+        StartCoroutine(WaitForVideoEnd());
 
     }
 
